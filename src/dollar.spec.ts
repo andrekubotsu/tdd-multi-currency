92352c27e8955f1de4a8e1d87817ad37524b9966
Andre Kubotsu <contato201@gmail.com>
2022-06-12 17:15:10 -0300

    test: added euro class test

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,11 +1,19 @@
 import { Dollar } from './dollar'
+import { Euro } from './euro'
 
-describe('Dollar', () => {
+describe('Money', () => {
   it('should return 10 when five dollars multiplied by 2 and 15 when multiplied by 3', () => {
     const five: Dollar = new Dollar(5)
 
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
+  })
+
+  it('should return 10 when five euros multiplied by 2 and 15 when multiplied by 3', () => {
+    const five: Euro = new Euro(5)
+
+    expect(five.times(2).equals(new Euro(10))).toBeTruthy()
+    expect(five.times(3).equals(new Euro(15))).toBeTruthy()
   })
 
   it('should return true when dollars of the same value are compared', () => {
