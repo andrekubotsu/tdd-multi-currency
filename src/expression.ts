11ae01805431f8e774059182dbde9117fdb22239
Andre Kubotsu <contato201@gmail.com>
2022-06-14 08:42:56 -0300

    feat: handle conversion rates

--- src/expression.ts
+++ src/expression.ts
@@ -1,6 +1,7 @@
+import { Bank } from './bank'
 import { Money } from './money'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Expression {
-    reduce: (to: string) => Money
+    reduce: (bank: Bank, to: string) => Money
 }
