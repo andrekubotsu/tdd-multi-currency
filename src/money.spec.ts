6f113f05fe8558e47b8fca3dd8c2164ac5b383cb
Andre Kubotsu <contato201@gmail.com>
2022-06-14 08:43:02 -0300

    test: handle conversion rates

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -54,4 +54,16 @@ describe('Money', () => {
 
     expect(result).toEqual(Money.dollar(1))
   })
+
+  it('should reduce from different currencies', () => {
+    const bank = new Bank()
+    bank.addRate('EUR', 'USD', 2)
+    const result = bank.reduce(Money.euro(2), 'USD')
+
+    expect(result).toEqual(Money.dollar(1))
+  })
+
+  it('should handle identity rate', () => {
+    expect(new Bank().rate('USD', 'USD')).toEqual(1)
+  })
 })
