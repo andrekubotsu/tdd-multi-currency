e10b8b6b6e2343c7dfa390074f8511f0a065ef42
Andre Kubotsu <contato201@gmail.com>
2022-06-14 10:15:01 -0300

    feat: implement missing operations

--- src/expression.ts
+++ src/expression.ts
@@ -5,4 +5,5 @@ import { Money } from './money'
 export interface Expression {
     reduce: (bank: Bank, to: string) => Money
     plus: (addend: Expression) => Expression
+    times: (multiplier: number) => Expression
 }
