d845dc50bddd753cca0008013f7da58a3284bc8d
Andre Kubotsu <contato201@gmail.com>
2022-06-14 10:07:31 -0300

    feat: get closer to abstraction

--- src/expression.ts
+++ src/expression.ts
@@ -4,4 +4,5 @@ import { Money } from './money'
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Expression {
     reduce: (bank: Bank, to: string) => Money
+    plus: (addend: Expression) => Expression
 }
